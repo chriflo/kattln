29167322be3db5b3d8bd5fe94f1aa60d39e15fcc
Florian Fehrenbacher <florian.fehrenbacher@sz.de>
2021-01-10 20:46:14 +0100

    introduce emotion styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,23 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <>
-      <label htmlFor="name">Name</label>
-      <input type="text" id="name" />
-      <Link href="/room/1">
-        <button>Runde erstellen</button>
-      </Link>
-      <button>Runde beitreten</button>
-    </>
+    <main
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <div css={{ width: '200px' }}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" />
+        <Link href="/room/1">
+          <button>Runde erstellen</button>
+        </Link>
+        <button>Runde beitreten</button>
+      </div>
+    </main>
   )
 }
