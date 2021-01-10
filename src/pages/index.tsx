b85d51608501e1626e37a941f97acea251661c54
Christian Brosche <christian@broschec.com>
2021-01-10 22:12:13 +0100

    Create room with random ID, save username in localStorage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
+import React from 'react'
 import Link from 'next/link'
 
+function createRoomId() {
+  return Math.random().toString(36).substring(2)
+}
+
 export default function Home() {
+  const [name, setName] = React.useState('')
+
+  React.useEffect(() => {
+    const savedName = window.localStorage.getItem('name')
+    if (savedName) setName(savedName)
+  }, [])
+
   return (
     <main
       css={{
@@ -13,9 +25,14 @@ export default function Home() {
     >
       <div css={{ width: '200px' }}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-        <Link href="/room/1">
-          <button>Runde erstellen</button>
+        <input
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          type="text"
+          id="name"
+        />
+        <Link href={`/room/${createRoomId()}`}>
+          <button onClick={() => window.localStorage.setItem('name', name)}>Runde erstellen</button>
         </Link>
         <button>Runde beitreten</button>
       </div>
--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -1,12 +1,26 @@
+import React from 'react'
 import { useRouter } from 'next/router'
 
 export default function Room() {
+  const [name, setName] = React.useState('')
   const router = useRouter()
+
+  React.useEffect(() => {
+    const savedName = window.localStorage.getItem('name')
+    if (!savedName) {
+      router.push('/')
+    } else {
+      setName(savedName)
+    }
+  }, [])
+
   const { id } = router.query
 
   return (
     <>
-      <h1>Runde - {id}</h1>
+      <h1>
+        Hi {name}! Runde - {id}
+      </h1>
     </>
   )
 }
