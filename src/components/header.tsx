52478348e0891e49fd0e4a880c1b183a5c27da19
Florian Fehrenbacher <florian.fehrenbacher@sz.de>
2021-01-11 18:23:56 +0100

    well one more try :D

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,18 +4,22 @@ import { colors } from 'styles/global'
 
 export function Header(props: any) {
   return (
-    <header css={headerStyles}>
-      <h1 {...props}>Kattln</h1>
+    <header css={headerStyles} {...props}>
+      <h1>Kattln</h1>
       <img src="/cards.svg" css={{ width: 50, position: 'fixed', top: 5, right: 5, zIndex: 1 }} />
     </header>
   )
 }
 
+export const headerHeight = 65
 const headerStyles = css`
+  position: fixed;
+  top: 0;
   text-align: center;
   font-size: 32px;
   letter-spacing: 8px;
   width: 100%;
+  height: ${headerHeight}px;
   padding: 16px;
   background: ${colors.blue};
   color: ${colors.mint};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { Header } from 'components/header'
+import { Header, headerHeight } from 'components/header'
 import 'normalize.css'
 import { GlobalStyles } from 'styles/global'
 import Head from 'next/head'
@@ -14,20 +14,20 @@ export default function MyApp({ Component, pageProps }) {
         flexDirection: 'column',
         position: 'fixed',
         bottom: 0,
+        paddingTop: headerHeight,
       }}
     >
       <Head>
         <link rel="icon" type="image/svg+xml" href="/leave.svg" />
       </Head>
       <GlobalStyles />
-      <Header css={{ alignSelf: 'flex-start' }} />
+      <Header />
       <main
         css={{
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-          height: '100vh',
           flexGrow: 1,
         }}
       >
