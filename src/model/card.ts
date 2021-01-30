bed93d25196477e393d0d0b61b8b0cc7dcdf12e3
Christian Brosche <christian@broschec.com>
2021-01-30 16:57:18 +0100

    feat: add values to cards

--- src/model/card.ts
+++ src/model/card.ts
@@ -1,18 +1,39 @@
 import shuffle from 'shuffle-array'
 export type Icon = 'eichel' | 'blatt' | 'herz' | 'schelle'
+
+type CardType = {
+  name: CardName
+  value: number
+}
+
 export type CardName = 'A' | 'K' | 'O' | 'U' | '10' | '9' | '8' | '7'
 
 export interface Card {
   name: CardName
   icon: Icon
   id: string
+  value: number
 }
 
 export const allIcons: Icon[] = ['eichel', 'blatt', 'herz', 'schelle']
-const allNames: CardName[] = ['A', 'K', 'O', 'U', '10', '9', '8', '7']
+const allTypes: CardType[] = [
+  { name: 'A', value: 11 },
+  { name: 'K', value: 4 },
+  { name: 'O', value: 3 },
+  { name: 'U', value: 2 },
+  { name: '10', value: 10 },
+  { name: '9', value: 0 },
+  { name: '8', value: 0 },
+  { name: '7', value: 0 },
+]
 
 const allCards: Card[] = allIcons.flatMap((icon) => {
-  return allNames.map((name) => ({ icon, name, id: generateId(name, icon) }))
+  return allTypes.map((cardType) => ({
+    icon,
+    name: cardType.name,
+    id: generateId(cardType.name, icon),
+    value: cardType.value,
+  }))
 })
 
 export function shuffleCards(): Card[] {
